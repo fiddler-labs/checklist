13dc12f2b50ce863fd959ad5ffd8f461a8916b9f
Amal Iyer <amal@fiddler.ai>
2023-09-11 15:06:39 -0700

    Removing subprocess call to install jupyter notebook

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ from subprocess import check_call
 import sys
 import os
 def enable_visual_interface():
-    check_call(f'"{sys.executable}"'+" -m pip install jupyter", shell=True)
     # Disbaling installation for PEP 518 compatibility
+    # check_call(f'"{sys.executable}"'+" -m pip install jupyter", shell=True)
     # import notebook
     # notebook.nbextensions.install_nbextension_python(
     #    "checklist.viewer", user=True, overwrite=True)
     # notebook.nbextensions.enable_nbextension_python(
     #    "checklist.viewer")
+    return
 
 def enable_visual_interface_shell_cmd(direction):
     sys.path.append(direction)
